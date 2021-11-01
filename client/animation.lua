1e0eae71757f7bbe916b971fe913058d9347f7bd
Eralp <eralpozcans@gmail.com>
2021-11-01 18:04:34 +0300

    PhoneProp native
    #133

--- client/animation.lua
+++ client/animation.lua
@@ -59,7 +59,7 @@ end
 
 function deletePhone()
 	if phoneProp ~= 0 then
-		Citizen.InvokeNative(0xAE3CBE5BF394C9C9 , Citizen.PointerValueIntInitialized(phoneProp))
+		DeleteObject(phoneProp)
 		phoneProp = 0
 	end
 end
