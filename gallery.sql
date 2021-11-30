cdff7f4af86228846b79d6cdd796f8c1dcbf8c67
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-11-29 20:12:56 -0600

    Delete gallery.sql

--- gallery.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `phone_gallery` (
-     `citizenid` VARCHAR(255) NOT NULL , 
-     `image` VARCHAR(255) NOT NULL ,
-     `date` timestamp NULL DEFAULT current_timestamp()
-     ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
