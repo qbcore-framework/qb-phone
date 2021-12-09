6fe044c151012b2a6a70e11dbb397ef3b4742409
Sky <hendersongaming4@gmail.com>
2021-12-09 15:12:34 -0600

    Removed Gallery sql because its in qbcore.sql

--- qb-phone.sql
+++ qb-phone.sql
@@ -79,9 +79,3 @@ CREATE TABLE IF NOT EXISTS `player_vehicles` (
   KEY `citizenid` (`citizenid`),
   KEY `license` (`license`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
-
-CREATE TABLE `phone_gallery` (
-   `citizenid` VARCHAR(255) NOT NULL , 
-   `image` VARCHAR(255) NOT NULL ,
-   `date` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
