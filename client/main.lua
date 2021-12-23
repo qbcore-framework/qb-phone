4ff58b8b7bb80dbf36166da6f30f86629f6c7982
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-12-23 16:17:23 +0200

    Merge file animation and use RequestAnimDict
    QBCore.Functions.RequestAnimDict

--- client/main.lua
+++ client/main.lua
@@ -1,6 +1,9 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local PlayerJob = {}
 local patt = "[?!@#]"
+local phoneProp = 0
+local phoneModel = `prop_npc_phone_02`
+
 PhoneData = {
     MetaData = {},
     isOpen = false,
@@ -27,6 +30,54 @@ PhoneData = {
 }
 
 -- Functions
+
+local function CheckAnimLoop()
+    CreateThread(function()
+        while PhoneData.AnimationData.lib ~= nil and PhoneData.AnimationData.anim ~= nil do
+            if not IsEntityPlayingAnim(PlayerPedId(), PhoneData.AnimationData.lib, PhoneData.AnimationData.anim, 3) then
+                QBCore.Functions.RequestAnimDict(PhoneData.AnimationData.lib)
+                TaskPlayAnim(PlayerPedId(), PhoneData.AnimationData.lib, PhoneData.AnimationData.anim, 3.0, 3.0, -1, 50, 0, false, false, false)
+            end
+            Wait(500)
+        end
+    end)
+end
+
+local function DoPhoneAnimation(anim)
+    local AnimationLib = 'cellphone@'
+    local AnimationStatus = anim
+    if IsPedInAnyVehicle(PlayerPedId(), false) then
+        AnimationLib = 'anim@cellphone@in_car@ps'
+    end
+    QBCore.Functions.RequestAnimDict(AnimationLib)
+    TaskPlayAnim(PlayerPedId(), AnimationLib, AnimationStatus, 3.0, 3.0, -1, 50, 0, false, false, false)
+    PhoneData.AnimationData.lib = AnimationLib
+    PhoneData.AnimationData.anim = AnimationStatus
+    CheckAnimLoop()
+end
+
+local function deletePhone()
+	if phoneProp ~= 0 then
+		DeleteObject(phoneProp)
+		phoneProp = 0
+	end
+end
+
+local function newPhoneProp()
+	deletePhone()
+	RequestModel(phoneModel)
+	while not HasModelLoaded(phoneModel) do
+		Wait(1)
+	end
+	phoneProp = CreateObject(phoneModel, 1.0, 1.0, 1.0, 1, 1, 0)
+
+	local bone = GetPedBoneIndex(PlayerPedId(), 28422)
+	if phoneModel == `prop_cs_phone_01` then
+		AttachEntityToEntity(phoneProp, PlayerPedId(), bone, 0.0, 0.0, 0.0, 50.0, 320.0, 50.0, 1, 1, 0, 0, 2, 1)
+	else
+		AttachEntityToEntity(phoneProp, PlayerPedId(), bone, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0, 0, 2, 1)
+	end
+end
 
 function string:split(delimiter)
     local result = { }
