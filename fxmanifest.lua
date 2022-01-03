94aa2dc5303880fc732e2234aa768bfbbe423d2a
Demo <51279632+Demo4889@users.noreply.github.com>
2022-01-03 14:51:15 -0500

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,7 +9,7 @@ ui_page 'html/index.html'
 shared_scripts {
     'config.lua',
     '@qb-apartments/config.lua',
-    '@MojiaGarages/config.lua',
+    '@qb-garages/config.lua',
 }
 
 client_scripts {
