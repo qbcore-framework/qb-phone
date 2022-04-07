ac0a543d4a65b64cf127411eb53b79b4c11a2344
MTL | Alex <92187331+divide29@users.noreply.github.com>
2022-04-07 18:52:23 +0200

    Textmessage is now being deleted after submit
    Solves issue #229

--- html/js/whatsapp.js
+++ html/js/whatsapp.js
@@ -196,6 +196,7 @@ $(document).on('click', '#whatsapp-openedchat-send', function(e){
             ChatType: "message",
         }));
         $("#whatsapp-openedchat-message").val("");
+        $("div.emojionearea-editor").data("emojioneArea").setText('');
     } else {
         QB.Phone.Notifications.Add("fab fa-whatsapp", "Whatsapp", "You can't send a empty message!", "#25D366", 1750);
     }
